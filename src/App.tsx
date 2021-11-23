0f89005571084da3b815ccf536896cb31379ccc3
TRAN Nicolas <tran.nicolas28@gmail.com>
2021-11-23 23:27:21 +0100

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React, {FunctionComponent, useState, useEffect} from 'react';
+import Pokemon from './models/pokemon';
+import POKEMONS from './models/mock-pokemon';
+  
+const App: FunctionComponent = () => {
+ 
+    const [pokemons,setPokemons] = useState<Pokemon[]>([]);
+
+    useEffect(() => setPokemons(POKEMONS),[]);
+    
+    return (
+        <div>
+            <h1>Pokédex : </h1>
+            <p>Il y a {pokemons.length} pokémons dans le Pokédex !</p>
+        </div>
+    )
+}
+  
+export default App;
