b487cf5e75d17be187bf569c98a2e0297677b260
TRAN Nicolas <tran.nicolas28@gmail.com>
2021-11-24 22:20:10 +0100

    Props and JSX syntax seen

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
-import React, {FunctionComponent, useState, useEffect} from 'react';
-import Pokemon from './models/pokemon';
-import POKEMONS from './models/mock-pokemon';
+import React, {FunctionComponent} from 'react';
+import PokemonList from './page/pokemon-list';
   
 const App: FunctionComponent = () => {
- 
-    const [pokemons,setPokemons] = useState<Pokemon[]>([]);
-
-    useEffect(() => setPokemons(POKEMONS),[]);
-    
+   
     return (
-        <div>
-            <h1>Pokédex : </h1>
-            <p>Il y a {pokemons.length} pokémons dans le Pokédex !</p>
-        </div>
+       <PokemonList />
     )
 }
   
